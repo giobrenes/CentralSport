242697563a6b8cf4d143d53f4fbd0594893b1f17
giobrenes <140575720+giobrenes@users.noreply.github.com>
2024-06-16 16:00:05 -0600

    Initial commit

---
+++ src/test/java/com/CentralSport/CentralSport/CentralSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CentralSport.CentralSport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
